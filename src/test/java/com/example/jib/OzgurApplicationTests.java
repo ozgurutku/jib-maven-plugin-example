3f7b59b04ce3a1c26696a31dc4905f4f1e12bc98
ozgurutku <ozgurutku444@hotmail.com>
2022-03-12 14:08:18 +0300

    jib

---
+++ src/test/java/com/example/jib/OzgurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzgurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
